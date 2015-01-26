c0cda346f95b7a4f85a0bf2178e150a14076cd22
Chris Gonzalez <chris@cwgmedia.com>
2015-01-26 16:12:16 -0500

    Buildify steps for auto replace

---
+++ buildify.js
@@ -0,0 +1,92 @@
+var buildify = require('buildify');
+var scripts, styles;
+
+// extract all comments: /<!--[\s\S]*?-->/g
+// match anything between two (word)[\s\S]*(word)
+
+buildify('dev')
+    .load('index.html')
+    .perform(function(content) {
+        var i, j,
+            comments = content.match(/<!--[\s\S]*?-->/g);
+
+        // for each matching set of comments w/ filename
+        for (i = 0; i < comments.length; i+=2) {
+            var opening = comments[i],
+                closing = comments[i+1];
+
+            var regex = new RegExp('(' + opening + ')[\\s\\S]*' + '(' + closing + ')', 'gi');
+
+            var enclosed = content.match(regex)[0];
+
+            if (enclosed.indexOf('scripts') > -1) {
+                console.log('Found a script block!');
+
+                var scriptTags = enclosed.match(/src=\"[\s\S]*?\"/gi);
+
+                console.log(scriptTags)
+
+            } else if (enclosed.indexOf('styles') > -1) {
+                console.log('Found a styles block!');
+            }
+        }
+
+
+
+        for (i = 0; i < scriptStrings.length; i++) {
+            var scriptTags = scriptStrings[i].split('</script>');
+        }
+
+        for (i = 0; i < scripts.length; i++) {
+            scripts[i] = scripts[i].substring(scripts[i].indexOf('"') + 1, scripts[i].lastIndexOf('"'));
+        }
+
+        content = content.replace((/(<!-- scripts -->)[\s\S)]*(<!-- \/scripts -->)/mi), '<script src="{{scripts}}"></script>');
+
+        console.log("SCRIPTS FOUND: ");
+        console.log(scripts);
+
+        // find all styles
+        styles = content.match(/(<!-- styles -->)[\s\S)]*(<!-- \/styles -->)/mi);
+
+        styles = styles[0].split('/>');
+        styles = styles.slice(0, styles.length-1);
+
+        for (i = 0; i < styles.length; i++) {
+            styles[i] = styles[i].substring(styles[i].indexOf('href="') + 6, styles[i].lastIndexOf('"'));
+        }
+
+        console.log("STYLES FOUND: ");
+        console.log(styles);
+
+        content = content.replace((/(<!-- styles -->)[\s\S)]*(<!-- \/styles -->)/mi), '<link rel="stylesheet" type="text/css" href="{{stylesheet}}">');
+
+        return content;
+    })
+    .save('build.html');
+
+buildify('src')
+    .load(scripts[0])
+    .concat(scripts.slice())
+    .uglify()
+    .changeDir('../')
+    .save('dist/js/techspace-radar.min.js');
+
+
+buildify('src')
+    .load(styles[0])
+    .concat(styles.slice(1))
+    .cssmin()
+    .changeDir('../')
+    .save('dist/css/techspace-radar.min.css');
+
+
+buildify('src')
+    .wrap('build.html', {
+        scripts: 'js/techspace-radar.min.js',
+        stylesheet: 'css/techspace-radar.min.css'
+    })
+    .changeDir('../')
+    .save('dist/index.html');
+
+//regex (<!-- scripts -->)[\s\S)]*(<!-- \/scripts -->)
